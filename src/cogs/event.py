6ebc3a6649122c0a579ffe67b2a6421375c4daa7
balt-dev <heptor42+git@gmail.com>
2025-09-22 16:55:24 -0500

    cba

--- src/cogs/event.py
+++ src/cogs/event.py
@@ -21,7 +21,7 @@ class EventCog(commands.Cog, name='Events'):
     async def bot_check(self, ctx: Context):
         async with self.bot.db.conn.cursor() as cur:
             # this is risky but guaranteed int so i think it's fine?
-            await cur.execute(f'SELECT DISTINCT * FROM BLACKLISTEDUSERS WHERE id = ?;', ctx.author.id)
+            await cur.execute('SELECT blacklisted FROM users WHERE user_id = ?;', ctx.author.id)
             if len(await cur.fetchall()):
                 dm_channel = await self.bot.create_dm(ctx.author)
                 await dm_channel.send('''You can\'t use this bot, as you have been blacklisted.
