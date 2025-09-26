f9a1cb9c549fb675168d5f8437bfb5ee2e24c338
balt-dev <heptor42+git@gmail.com>
2025-09-26 11:59:56 -0500

    Staging script

---
+++ stage.sh
@@ -0,0 +1,8 @@
+CURR_BRANCH=git rev-parse --abbrev-ref HEAD
+ssh root@107.172.159.51 /bin/sh <<EOF  
+	cd /root/ric-staging
+	systemctl stop ric-staging
+	git pull -f
+	git checkout $CURR_BRANCH
+	systemctl start ric-staging
+EOF 
