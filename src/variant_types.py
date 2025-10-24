1f7e90f2976376f42387319c41d5d547b9994b83
balt-dev <heptor42+git@gmail.com>
2025-10-24 12:44:34 -0500

    Parsing types

---
+++ src/variant_types.py
@@ -0,0 +1,261 @@
+import glob
+import inspect
+from inspect import Parameter
+import math
+import types
+import typing
+from typing import Any, Literal, Optional, Union, get_origin, \
+                    get_args, Callable, Self, Type
+from pathlib import Path
+from dataclasses import dataclass, field
+from abc import ABC, abstractmethod
+
+import cv2
+import numpy as np
+import visual_center
+
+from . import constants, errors
+from .tile import Tile, TileSkeleton, TileData, ProcessedTile
+from .types import Bot, RenderContext, Renderer, SignText, NumpySprite
+
+@dataclass
+class AbstractVariantContext(ABC):
+    pass
+
+
+@dataclass
+class SkeletonVariantContext(AbstractVariantContext):
+    pass
+
+
+@dataclass
+class SignVariantContext(AbstractVariantContext):
+    bot: Bot
+    ctx: RenderContext
+    renderer: Renderer
+
+
+@dataclass
+class TileVariantContext(AbstractVariantContext):
+    tile_data_cache: dict[str, TileData]
+
+
+@dataclass
+class SpriteVariantContext(AbstractVariantContext):
+    tile: Tile
+    wobble: int
+    renderer: Renderer
+
+
+@dataclass
+class PostVariantContext(AbstractVariantContext):
+    pass
+
+
+#region Parser
+
+INT_REGEX = re.compile(
+    r'[+-]?(?:0x[0-9A-Fa-f]+|0o[0-7]+|0b[01]+|[1-9][0-9]*|0)',
+    re.IGNORECASE
+)
+
+
+def parse_int(_ty, string: str) -> tuple[str, int | None]:
+    match = INT_REGEX.match(string)
+    if match is None:
+        return string, None
+    try:
+        return string[match.end():], int(match.group(0), base=0)
+    except ValueError:
+        return string, None
+
+
+FLOAT_REGEX = re.compile(
+    r'[+-]?(((\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?)|inf|nan)',
+    re.IGNORECASE
+)
+
+
+def parse_float(_ty, string: str) -> tuple[str, float | None]:
+    match = FLOAT_REGEX.match(string)
+    if match is None:
+        return string, None
+    try:
+        return string[match.end():], float(match.group(0))
+    except ValueError:
+        return string, None
+
+
+def parse_bool(_ty, string: str) -> tuple[str, bool | None]:
+    if string.startswith("true") or string.startswith("True"):
+        return string[4:], True
+    elif string.startswith("false") or string.startswith("False"):
+        return string[5:], False
+    else:
+        return string, None
+
+
+def parse_literal(ty, string: str) -> tuple[str, str | None]:
+    valid_values = ty.__args__
+    for value in valid_values:
+        if string.startswith(value):
+            return string.removeprefix(value), value
+    return string, None
+
+
+type_parsers = {
+    int: parse_int,
+    float: parse_float,
+    bool: parse_bool,
+    typing._LiteralGenericAlias: parse_literal,
+}
+
+#endregion
+
+
+@dataclass
+class AbstractVariantFactory(ABC):
+    identifier: str
+    description: str
+    syntax_description: str
+    parser: Callable[[str], Self | None]
+    target: Type
+    context: Type
+    applicator: Callable[[Any, Any, ...], None]
+    hashable: bool = True
+    nameless: bool = False
+
+    @property
+    @abstractmethod
+    def type(self):
+        raise NotImplementedError("cannot get type of abstract variant factory")
+
+    @classmethod
+    def define_variant(
+        cls, func: Callable, *,
+        names: tuple[str] | None = (),
+        hashable: bool = True
+    ):
+        """Dynamically defines a Variant subclass based on an annotated function definition."""
+        global ALL_VARIANTS
+
+        # This code is kind of weird.
+        # Basically, we create an opaque subclass of the
+        # class that we're calling this from only when
+        # this module is loaded.
+
+        identifier = func.__name__
+        description = func.__doc__
+        ty = cls.type
+        assert description is not None, \
+            f"Variant `{identifier}` is missing a docstring."
+
+        signature = inspect.signature(func)
+        params: tuple[tuple[str, Parameter], ...] = tuple((name, value) for name, value in signature.parameters.items())
+        assert len(params) >= 2, f"Variant `{identifier}` must have at least two parameters."
+
+        # Check all parameters are annotated
+        for param in params:
+            if param[1].annotation is Parameter.empty:
+                raise AssertionError(f"Parameter `{param[0]}` of variant `{identifier}` has no type annotation.")
+        # Sanity check the first two parameters
+        assert params[0][1].annotation == cls.target, \
+            f"Variant `{identifier}` has an incorrectly annotated target."
+        assert params[1][1].annotation == cls.context, \
+            f"Variant `{identifier}` has an incorrectly annotated context."
+
+        params = params[2:]
+        # Generate parser
+        parser = cls.generate_parser(names, params)
+        syntax_description = cls.generate_syntax_description(names, params)
+
+        # Generate the subclass
+        variant = type(
+            identifier,
+            (cls, ),
+            dict(
+                identifier = identifier,
+                description = description,
+                applicator = func,
+                syntax_description = syntax_description,
+                parser = parser,
+                hashable = hashable,
+                nameless = names is None
+            )
+        )
+
+        ALL_VARIANTS[identifier] = variant
+
+        return variant
+
+    def generate_parser(
+        names: tuple[str] | None,
+        params: tuple[tuple[str, Parameter], ...]
+    ) -> Callable[[str], Self | None]:
+        print(params)
+        return None
+
+    def generate_syntax_description(
+        names: tuple[str] | None,
+        params: tuple[tuple[str, Parameter], ...]
+    ) -> str:
+        return "<todo>"
+
+
+@dataclass
+class SkeletonVariantFactory(AbstractVariantFactory):
+    applicator: Callable[[TileSkeleton, Any, ...], None] = lambda *_: None
+    target: Type = TileSkeleton
+    context: Type = SkeletonVariantContext
+    type: str = "skel"
+
+
+@dataclass
+class SignVariantFactory(AbstractVariantFactory):
+    applicator: Callable[[SignText, Any, ...], None] = lambda *_: None
+    target: Type = SignText
+    context: Type = SignVariantContext
+    type: str = "sign"
+
+
+@dataclass
+class TileVariantFactory(AbstractVariantFactory):
+    applicator: Callable[[Tile, Any, ...], None] = lambda *_: None
+    target: Type = Tile
+    context: Type = TileVariantContext
+    type: str = "tile"
+
+
+@dataclass
+class SpriteVariantFactory(AbstractVariantFactory):
+    applicator: Callable[[NumpySprite, Any, ...], None] = lambda *_: None
+    target: Type = NumpySprite
+    context: Type = SpriteVariantContext
+    type: str = "sprite"
+
+
+@dataclass
+class PostVariantFactory(AbstractVariantFactory):
+    applicator: Callable[[ProcessedTile, Any, ...], None] = lambda *_: None
+    target: Type = ProcessedTile
+    context: Type = PostVariantContext
+    type: str = "post"
+
+
+@dataclass
+class Variant:
+    args: tuple
+    factory: AbstractVariantFactory
+    persistent: bool
+
+    def __hash__(self):
+        if not self.factory.hashable:
+            return id(self)
+        return hash((self.factory.name, *self.args))
+
+    @property
+    def type(self):
+        return self.factory.type
+
+    def apply(self, target: Any, context: AbstractVariantContext):
+        self.factory.applicator(target, context, *self.args)
